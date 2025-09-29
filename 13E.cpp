51c37f99e8dbfa0daf0f00b34baf6f381595dae9
gawalechetan78-stack <gawalechetan78@gmail.com>
2025-09-29 23:36:00 +0530

    13E.cpp

---
+++ 13E.cpp
@@ -0,0 +1,38 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+class Complex
+{
+private:
+    int real;
+    int img;
+
+public:
+    Complex(int r = 0, int i = 0)
+    {
+        real = r;
+        img = i;
+    }
+
+    Complex operator+(Complex const &obj)
+    {
+        return Complex(real + obj.real, img + obj.img);
+    }
+
+    void display() {
+        cout << real << " +i" << img << endl;
+    }
+};
+
+int main(void)
+{
+    Complex c1(10, 5), c2(15, 20);
+
+    Complex c3 = c1 + c2;
+    c3.display();
+
+    return 0;
+}
